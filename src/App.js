2629b1ab8d72ce914f6af921c5f83541af95b204
Shanu30 <ksechenari@gmail.com>
2022-03-04 21:18:55 +0530

    navbar integrated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import BookYourStay from './components/BookYourStay';
 import LiveItNow from './components/LiveItNow';
+import Navbar from './components/Navbar/Navbar';
 import SelectUnitGridImage from './components/SelectUnitGridImage';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <LiveItNow />
       <SelectUnitGridImage/>
       <BookYourStay />
