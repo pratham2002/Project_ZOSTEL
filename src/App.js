5fb6db93e7b6ad0a4089f8c776caaaac86dc5d49
Shanu30 <ksechenari@gmail.com>
2022-03-05 05:30:23 +0530

    first carousel added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import BookYourStay from './components/BookYourStay';
 import LiveItNow from './components/LiveItNow';
 import Navbar from './components/Navbar/Navbar';
 import SelectUnitGridImage from './components/SelectUnitGridImage';
+import Sec1 from './components/Carousel1/Sec1'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <LiveItNow />
+      <Sec1 />
       <SelectUnitGridImage/>
       <BookYourStay />
     </div>
